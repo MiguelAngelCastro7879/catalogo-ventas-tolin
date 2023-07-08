74d8d2bcbac1ef6974d754b820ee58b1cf0b2271
MiguelAngelCastro <mikeangel7879@gmail.com>
2023-07-07 18:27:58 -0600

    [Create]  project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as  Routes, Route } from "react-router-dom";
+import Dashboard from './modules/Dashboard'
 
 function App() {
   return (
---
+++ src/modules/Dashboard.jsx
@@ -0,0 +1,9 @@
+function Dashboard() {
+  return (
+    <div>
+        HOLA MUNDOOOOOO
+    </div>
+  );
+}
+
+export default Dashboard;
